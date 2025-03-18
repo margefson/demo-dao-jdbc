593fd88dedc95d78decedfc27d485c6346f9482c
Margefson <margefson@gmail.com>
2025-03-18 14:21:45 -0400

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -36,5 +36,12 @@ public class Program {
 		Seller newSeller = new Seller(null, "Greg", "greg@gail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New Id = " + newSeller.getId());
+		
+		System.out.println("\n==== TEST 5: seller update =====");
+		seller = sellerDao.findById(1);
+		seller.setName("Machado");
+		seller.setEmail("machado@gmail.com");
+		sellerDao.update(seller);
+		System.out.println("Update completed");
 	}
 }
